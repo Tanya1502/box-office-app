a109077f0bdadd48e2af8d5a6238b14f6d592efe
Tanya <tannutanyaverma@gmail.com>
2023-03-19 10:35:12 -0500

    installed react-query. replaced useShowById by useQuery

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getShowById } from '../api/tvmaze';
 import { useState } from 'react';
 
@@ -7,24 +8,28 @@ const useShowById = showId => {
   const [showData, setShowData] = useState(null);
   const [showError, setShowError] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-    fetchData();
-  }, [showId]);
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       try {
+  //         const data = await getShowById(showId);
+  //         setShowData(data);
+  //       } catch (error) {
+  //         setShowError(error);
+  //       }
+  //     }
+  //     fetchData();
+  //   }, [showId]);
 
-  return { showData, showError };
+  //   return { showData, showError };
 };
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  // const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: ${showError.message} </div>;
