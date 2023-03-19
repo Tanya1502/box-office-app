d81e26b16f30fdc3bd41ac727fea8040e78e3fcb
Tanya <tannutanyaverma@gmail.com>
2023-03-19 09:08:39 -0500

    fetch data inside show component

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,9 +1,35 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getShowById } from '../api/tvmaze';
+import { useState } from 'react';
 
 const Show = () => {
   const { showId } = useParams();
 
-  return <div>Show Page for {showId}</div>;
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (error) {
+        setShowError(error);
+      }
+    }
+    fetchData();
+  }, [showId]);
+
+  if (showError) {
+    return <div>We have an error: ${showError.message} </div>;
+  }
+
+  if (showData) {
+    return <div>Got show data: ${showData.name}</div>;
+  }
+
+  return <div>Data is loading</div>;
 };
 
 export default Show;
